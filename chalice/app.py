0a7af31a7d427e49ad832ac0e73ab2135cdf5ade
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2018-08-22 12:07:20 +0100

    Implement auth check on every template load

--- chalice/app.py
+++ chalice/app.py
@@ -1,13 +1,16 @@
-from chalice import Chalice
+import magic
+import os
+# from urllib.parse import urlparse, parse_qs
+
+from chalice import Chalice, Response, BadRequestError
+
 from chalicelib.models import DatabaseHandle
+# from chalicelib.auth import AuthHandler
+from chalicelib.views import TemplateHandler
 
 
 app = Chalice(app_name='cloud-security-watch')
-
-
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+# app.auth_handler = AuthHandler()
 
 
 # native lambda admin function to be invoked
@@ -78,6 +81,84 @@ def database_run(event, context):
         status = False
 
     return status
+
+
+@app.route('/')
+def index():
+
+    templates = TemplateHandler()
+    request = app.current_request
+    response = templates.render_authorized_route_template('/', request)
+
+    return Response(**response)
+
+
+@app.route('/assets/{proxy+}')
+def asset_render():
+    try:
+        req = app.current_request
+        proxy = req.uri_params['proxy']
+
+        binary_types = [
+            'application/octet-stream',
+            'image/webp',
+            'image/apng',
+            'image/png',
+            'image/svg',
+            'image/jpeg',
+            'image/x-icon',
+            'image/vnd.microsoft.icon',
+            'application/x-font-woff',
+            'font/woff',
+            'font/woff2',
+            'font/eot'
+        ]
+
+        ascii_types = [
+            'text/plain',
+            'text/css',
+            'text/javascript'
+        ]
+
+        true_path = os.path.join(os.path.dirname(__file__), 'chalicelib', 'templates', proxy)
+
+        if ".." in proxy:
+            raise Exception(f"No back (..) navigating: {proxy}")
+
+        mime_type = get_mime_type(true_path)
+
+        if mime_type in ascii_types:
+            with open(true_path, 'r') as text:
+                data = text.read()
+        elif mime_type in binary_types:
+            with open(true_path, 'rb') as img:
+                data = img.read()
+        else:
+            raise Exception(f"Unsupported file type: {mime_type}")
+
+        return Response(
+            body=data,
+            status_code=200,
+            headers={"Content-Type": mime_type}
+        )
+    except Exception as e:
+        raise BadRequestError(str(e))
+
+
+def get_mime_type(file):
+    mime = magic.Magic(mime=True)
+    mime_type = mime.from_file(file)
+    file_name, ext = os.path.splitext(file)
+
+    if ext == '.js':
+        mime_type = 'text/javascript'
+    elif ext == '.css':
+        mime_type = 'text/css'
+    elif ext == '.png':
+        mime_type = 'image/png'
+
+    return mime_type
+
 
 # The view function above will return {"hello": "world"}
 # whenever you make an HTTP GET request to '/'.
