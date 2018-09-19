9f9f52ea09c7fa94822720d9e9d56de348ec7158
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2018-09-19 08:43:55 +0100

    Correct logout route definition

--- chalice/app.py
+++ chalice/app.py
@@ -54,25 +54,17 @@ def load_route_services():
 @app.route('/')
 def index():
 
-    app.log.debug('Try loading route services')
-
     load_route_services()
-
-    app.log.debug('Loaded route services')
 
     response = app.templates.render_authorized_route_template('/', app.current_request)
 
     return Response(**response)
 
 
-@app.route('/')
-def index():
-
-    app.log.debug('Try loading route services')
+@app.route('/logout')
+def logout():
 
     load_route_services()
-
-    app.log.debug('Loaded route services')
 
     response = app.templates.render_authorized_route_template('/logout', app.current_request)
 
