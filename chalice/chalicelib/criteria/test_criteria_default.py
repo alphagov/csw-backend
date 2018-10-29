c3ce922d7112ea72425e5830dbbbbba6552484e8
Argyris Galamatis <argyris.galamatis@digital.cabinet-office.gov.uk>
2018-10-29 17:10:29 +0000

    hasattr assertion and grouping related sub tests

--- chalice/chalicelib/criteria/test_criteria_default.py
+++ chalice/chalicelib/criteria/test_criteria_default.py
@@ -50,26 +50,37 @@ class TestCriteriaDefault(unittest.TestCase):
             'GdsAwsClient'
         )
         # vars initialized with constants on class definition
-        self.assertFalse(self.criteria_default.active)
-        self.assertDictEqual(self.criteria_default.resources, {})
-        self.assertSequenceEqual(
-            self.criteria_default.resource_type, 'AWS::*::*'
-        )
-        self.assertSequenceEqual(self.criteria_default.annotation, '')
-        self.assertEqual(
-            self.criteria_default.ClientClass, gds_aws_client_class
-        )
-        self.assertIsNone(self.criteria_default.title)
-        self.assertIsNone(self.criteria_default.description)
-        self.assertIsNone(self.criteria_default.why_is_it_important)
-        self.assertIsNone(self.criteria_default.how_do_i_fix_it)
+        with self.subTest():
+            self.assertFalse(self.criteria_default.active)
+        with self.subTest():
+            self.assertDictEqual(self.criteria_default.resources, {})
+        with self.subTest():
+            self.assertSequenceEqual(
+                self.criteria_default.resource_type, 'AWS::*::*'
+            )
+        with self.subTest():
+            self.assertSequenceEqual(self.criteria_default.annotation, '')
+        with self.subTest():
+            self.assertEqual(
+                self.criteria_default.ClientClass, gds_aws_client_class
+            )
+        with self.subTest():
+            self.assertIsNone(self.criteria_default.title)
+        with self.subTest():
+            self.assertIsNone(self.criteria_default.description)
+        with self.subTest():
+            self.assertIsNone(self.criteria_default.why_is_it_important)
+        with self.subTest():
+            self.assertIsNone(self.criteria_default.how_do_i_fix_it)
         # vars initialized algorithmically at object instantiation
-        self.assertIsInstance(
-            self.criteria_default.app, Chalice
-        )
-        self.assertIsInstance(
-            self.criteria_default.client, gds_aws_client_class
-        )
+        with self.subTest():
+            self.assertIsInstance(
+                self.criteria_default.app, Chalice
+            )
+        with self.subTest():
+            self.assertIsInstance(
+                self.criteria_default.client, gds_aws_client_class
+            )
 
     def test_get_session(self):
         """
@@ -83,12 +94,14 @@ class TestCriteriaDefault(unittest.TestCase):
         test the describe method for success and failure
         """
         describe = self.criteria_default.describe()
-        self.assertIsInstance(describe, dict)
+        with self.subTest():
+            self.assertIsInstance(describe, dict)
         keys = [
             'title', 'description', 'why_is_it_important', 'how_do_i_fix_it',
         ]
         for key in describe:
-            self.assertIn(key, keys)
+            with self.subTest(key=key):
+                self.assertIn(key, keys)
 
     def test_get_data(self):
         """
@@ -108,52 +121,69 @@ class TestCriteriaDefault(unittest.TestCase):
         built_evaluation = self.criteria_default.build_evaluation(
             resource_id, compliance_type, event, resource_type
         )
-        self.assertIsInstance(built_evaluation, dict)
+        with self.subTest():
+            self.assertIsInstance(built_evaluation, dict)
         keys = [
             'resource_type', 'resource_id', 'compliance_type',
             'is_compliant', 'is_applicable', 'status_id',
         ]
         for key in built_evaluation:
-            self.assertIn(key, keys)
+            with self.subTest(key=key):
+                self.assertIn(key, keys)
         # recall the method with the annotation optional param
         annotation = 'optional_string'
         built_evaluation = self.criteria_default.build_evaluation(
             resource_id, compliance_type, event, resource_type, annotation
         )
-        self.assertIsInstance(built_evaluation, dict)
-        self.assertIn('annotation', built_evaluation)
+        with self.subTest():
+            self.assertIsInstance(built_evaluation, dict)
+        with self.subTest():
+            self.assertIn('annotation', built_evaluation)
         # return values tests for type correctness and values when possible
-        self.assertEqual(built_evaluation['annotation'], annotation)
-        self.assertEqual(built_evaluation['resource_type'], resource_type)
-        self.assertEqual(built_evaluation['resource_id'], resource_id)
-        self.assertEqual(built_evaluation['compliance_type'], compliance_type)
-        self.assertTrue(built_evaluation['is_compliant'])
-        self.assertTrue(built_evaluation['is_applicable'])
-        self.assertEqual(
-            built_evaluation['status_id'],
-            self.criteria_default.get_status(built_evaluation)
-        )
+        with self.subTest():
+            self.assertEqual(built_evaluation['annotation'], annotation)
+        with self.subTest():
+            self.assertEqual(built_evaluation['resource_type'], resource_type)
+        with self.subTest():
+            self.assertEqual(built_evaluation['resource_id'], resource_id)
+        with self.subTest():
+            self.assertEqual(built_evaluation['compliance_type'], compliance_type)
+        with self.subTest():
+            self.assertTrue(built_evaluation['is_compliant'])
+        with self.subTest():
+            self.assertTrue(built_evaluation['is_applicable'])
+        with self.subTest():
+            self.assertEqual(
+                built_evaluation['status_id'],
+                self.criteria_default.get_status(built_evaluation)
+            )
         # finally the remaining cases of compliance_type
         compliance_type = 'NON_COMPLIANT'
         built_evaluation = self.criteria_default.build_evaluation(
             resource_id, compliance_type, event, resource_type, annotation
         )
-        self.assertFalse(built_evaluation['is_compliant'])
-        self.assertTrue(built_evaluation['is_applicable'])
-        self.assertEqual(
-            built_evaluation['status_id'],
-            self.criteria_default.get_status(built_evaluation)
-        )
+        with self.subTest():
+            self.assertFalse(built_evaluation['is_compliant'])
+        with self.subTest():
+            self.assertTrue(built_evaluation['is_applicable'])
+        with self.subTest():
+            self.assertEqual(
+                built_evaluation['status_id'],
+                self.criteria_default.get_status(built_evaluation)
+            )
         compliance_type = 'NOT_APPLICABLE'
         built_evaluation = self.criteria_default.build_evaluation(
             resource_id, compliance_type, event, resource_type, annotation
         )
-        self.assertFalse(built_evaluation['is_compliant'])
-        self.assertFalse(built_evaluation['is_applicable'])
-        self.assertEqual(
-            built_evaluation['status_id'],
-            self.criteria_default.get_status(built_evaluation)
-        )
+        with self.subTest():
+            self.assertFalse(built_evaluation['is_compliant'])
+        with self.subTest():
+            self.assertFalse(built_evaluation['is_applicable'])
+        with self.subTest():
+            self.assertEqual(
+                built_evaluation['status_id'],
+                self.criteria_default.get_status(built_evaluation)
+            )
 
     def test_get_status(self):
         """
@@ -166,33 +196,49 @@ class TestCriteriaDefault(unittest.TestCase):
             {'is_compliant': True, 'is_applicable': True, },
         ]
         # test for return of 2 implying a
-        self.assertEqual(
-            self.criteria_default.get_status(eval_dicts[0]), 2
-        )
-        self.assertEqual(
-            self.criteria_default.get_status(eval_dicts[2]), 2
-        )
-        self.assertEqual(
-            self.criteria_default.get_status(eval_dicts[3]), 2
-        )
+        with self.subTest():
+            self.assertEqual(
+                self.criteria_default.get_status(eval_dicts[0]), 2
+            )
+        with self.subTest():
+            self.assertEqual(
+                self.criteria_default.get_status(eval_dicts[2]), 2
+            )
+        with self.subTest():
+            self.assertEqual(
+                self.criteria_default.get_status(eval_dicts[3]), 2
+            )
         # test for fail which returns 3
-        self.assertEqual(
-            self.criteria_default.get_status(eval_dicts[1]), 3
-        )
+        with self.subTest():
+            self.assertEqual(
+                self.criteria_default.get_status(eval_dicts[1]), 3
+            )
 
     def test_empty_summary(self):
         """
         test the empty_summary method
         """
-        self.assertEqual(
-            self.criteria_default.empty_summary(), EMPTY_SUMMARY
-        )
+        with self.subTest():
+            self.assertEqual(
+                self.criteria_default.empty_summary(),
+                EMPTY_SUMMARY,
+                msg='empty summary does not match the prototype'
+            )
 
 
 class CriteriaSubclassTestCaseMixin(object):
     """
     Unit tests for all CriteriaDefault subclasses
     """
+
+    def assertHasAttr(self, obj, attr):
+        """
+        custom assertion method for hasattr built-in function
+        """
+        self.assertTrue(
+            hasattr(obj, attr),
+            msg="'{}' object has no attribute '{}'".format(obj, attr)
+        )
 
     @classmethod
     def setUpClass(cls):
@@ -221,36 +267,42 @@ class CriteriaSubclassTestCaseMixin(object):
             )
         # subclass specific attributes
         with self.subTest():
+            self.assertHasAttr(self.subclass, 'check_id')
             self.assertIsInstance(
                 self.subclass.check_id,
                 str,
                 msg='check_id must be a non-empty string'
             )
         with self.subTest():
+            self.assertHasAttr(self.subclass, 'check_id')
             self.assertGreater(
                 len(self.subclass.check_id),
                 0,
                 msg='check_id must be a non-empty string'
             )
         with self.subTest():
+            self.assertHasAttr(self.subclass, 'language')
             self.assertIsInstance(
                 self.subclass.language,
                 str,
                 msg='language must be a non-empty string'
             )
         with self.subTest():
+            self.assertHasAttr(self.subclass, 'language')
             self.assertGreater(
                 len(self.subclass.language),
                 0,
                 msg='language must be a non-empty string'
             )
         with self.subTest():
+            self.assertHasAttr(self.subclass, 'region')
             self.assertIsInstance(
                 self.subclass.region,
                 str,
                 msg='region must be a non-empty string'
             )
         with self.subTest():
+            self.assertHasAttr(self.subclass, 'region')
             self.assertGreater(
                 len(self.subclass.region),
                 0,
@@ -262,7 +314,6 @@ class CriteriaSubclassTestCaseMixin(object):
                 str,
                 msg='title must be a non-empty string'
             )
-        with self.subTest():
             self.assertGreater(len(
                 self.subclass.title),
                 0,
@@ -274,7 +325,6 @@ class CriteriaSubclassTestCaseMixin(object):
                 str,
                 msg='description must be a non-empty string'
             )
-        with self.subTest():
             self.assertGreater(
                 len(self.subclass.description),
                 0,
@@ -286,7 +336,6 @@ class CriteriaSubclassTestCaseMixin(object):
                 str,
                 msg='why_is_it_important must be a non-empty string'
             )
-        with self.subTest():
             self.assertGreater(
                 len(self.subclass.why_is_it_important),
                 0,
@@ -298,7 +347,6 @@ class CriteriaSubclassTestCaseMixin(object):
                 str,
                 msg='how_do_i_fix_it must be a non-empty string'
             )
-        with self.subTest():
             self.assertGreater(
                 len(self.subclass.how_do_i_fix_it),
                 0,
