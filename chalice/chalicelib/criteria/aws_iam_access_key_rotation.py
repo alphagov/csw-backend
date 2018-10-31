832af307263ea3b1d85bd851749c3d0a0355aa3e
Argyris Galamatis <argyris.galamatis@digital.cabinet-office.gov.uk>
2018-10-31 11:15:16 +0000

    key rotation implementation

--- chalice/chalicelib/criteria/aws_iam_access_key_rotation.py
+++ chalice/chalicelib/criteria/aws_iam_access_key_rotation.py
@@ -14,6 +14,16 @@ class AwsIamAccessKeyRotation(CriteriaDefault):
         """
         """
         super(AwsIamAccessKeyRotation, self).__init__(app)
+        self.active = True
+        self.resource_type = 'AWS::*::*'
+        self.check_id = 'DqdJqYeRm5'
+        self.language = 'en'
+        self.region = 'us-east-1'
+        self.annotation = ''
+        self.title = None
+        self.description = None
+        self.why_is_it_important = None
+        self.how_do_i_fix_it = None
 
     def get_data(self, session, **kwargs):
         """
@@ -24,8 +34,8 @@ class AwsIamAccessKeyRotation(CriteriaDefault):
         """
         """
         return {
-            'resource_id': None,
-            'resource_name': None,
+            'resource_id': 'root',
+            'resource_name': 'Root Account',
         }
 
     def evaluate(self, event, item, whitelist=[]):
@@ -35,8 +45,20 @@ class AwsIamAccessKeyRotation(CriteriaDefault):
         The item parameter is the value of the result key of the
         support API method called describe_trusted_advisor_check_result.
         """
-        # logic to determine resource_id, compliance_type
-        compliance_type = None
+        # compliance_type
+        compliance_type = 'NON_COMPLIANT'
+        if item['result']['status'] == 'ok':
+            compliance_type = 'COMPLIANT'
+        else:  # construct annotation
+            self.annotation = '<p>STATUS: {}</p><ol>'.format(
+                item['result']['status'].upper()
+            )
+            for flagged_resource in item['result']['flaggedResources']:
+                if flagged_resource['metadata'][0] != 'Green':
+                    self.annotation += '{} {} {} {} {}'.format(
+                        *flagged_resource['metadata']
+                    )
+            self.annotation += '</ol>'
         return self.build_evaluation(
             self.translate()['resource_id'],
             compliance_type,
