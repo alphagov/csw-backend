88b1c371fd2dfb06056cc3e4c40dca6964d5c6b8
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2018-11-30 18:35:26 +0000

    Try initialising the oauth flow in load route services.

--- chalice/app.py
+++ chalice/app.py
@@ -26,6 +26,8 @@ def load_route_services():
     try:
 
         app.auth = AuthHandler(app)
+        app.base_url = app.auth.get_base_url(app.current_request)
+        app.flow = app.auth.get_auth_flow(app.base_url)
 
         app.log.debug("Loaded auth")
 
