aef72613b3acd9b10d97c0d07d43ed79c9233054
Argyris Galamatis <argyris.galamatis@digital.cabinet-office.gov.uk>
2018-12-14 17:15:58 +0000

    ELB listener checks implemented

--- chalice/chalicelib/criteria/aws_elb_listener_security.py
+++ chalice/chalicelib/criteria/aws_elb_listener_security.py
@@ -52,14 +52,15 @@ class ELBListenerSecurityNoListener(ELBListenerSecurity):
 
     def __init__(self, app):
         self.title = 'ELB Listener Security'
-        self.description = (
-            ''
-        )
+        self.description = 'A load balancer does not have any listeners that use a secure protocol (HTTPS, SSL, etc)'
         self.why_is_it_important = (
-            ''
+            'If the listeners do not use a secure protocol, '
+            'the requests between your clients and the load balancer are unencrypted and less secure.'
         )
         self.how_do_i_fix_it = (
-            ''
+            'Either add an HTTPS listener with an up-to-date security policy to the ELB, '
+            'or edit an existing one to use HTTPS. Further instructions and information can be found here: '
+            'https://docs.aws.amazon.com/elasticloadbalancing/latest/application/create-https-listener.html'
         )
         super(ELBListenerSecurityNoListener, self).__init__(app)
 
@@ -68,9 +69,13 @@ class ELBListenerSecurityNoListener(ELBListenerSecurity):
         TODO
         """
         compliance_type = 'NON_COMPLIANT'
-        self.annotation = (
-            ''
-        )
+        if item["metadata"][4] == 'No listener uses a secure protocol':
+            self.annotation = (
+                f'"{item["metadata"][1]}" load balancer in region "{item["metadata"][0]}" with resource ID: '
+                f'"{item["resourceId"]}" has no listener that uses a secure protocol (HTTPS or SSL).'
+            )
+        else:
+            compliance_type = 'COMPLIANT'
         return self.build_evaluation(
             item['resourceId'],
             compliance_type,
@@ -88,14 +93,16 @@ class ELBListenerSecurityPredefinedOutdated(ELBListenerSecurity):
 
     def __init__(self, app):
         self.title = 'ELB Listener Security'
-        self.description = (
-            ''
-        )
+        self.description = 'The security policy on one of the listeners to a load balancer is outdated.'
         self.why_is_it_important = (
-            ''
+            'The security policy of a listener defines ciphers and protocols it uses when communicating with the ELB. '
+            'Policies get updated when protocols are found to be not as secure as once thought, '
+            'so an outdated security policy may be leave the connection between a listener and an ELB vulnerable.'
         )
         self.how_do_i_fix_it = (
-            ''
+            'Change the security policy on the listener to a more recent one. '
+            'Further instructions and information can be found here: '
+            'https://docs.aws.amazon.com/elasticloadbalancing/latest/application/create-https-listener.html'
         )
         super(ELBListenerSecurityPredefinedOutdated, self).__init__(app)
 
@@ -104,9 +111,13 @@ class ELBListenerSecurityPredefinedOutdated(ELBListenerSecurity):
         TODO
         """
         compliance_type = 'NON_COMPLIANT'
-        self.annotation = (
-            ''
-        )
+        if item["metadata"][4] == 'A listener uses an outdated predefined SSL security policy':
+            self.annotation = (
+                f'"{item["metadata"][1]}" load balancer in region "{item["metadata"][0]}" with resource ID: '
+                f'"{item["resourceId"]}" uses an outdated predefined SSL security policy.'
+            )
+        else:
+            compliance_type = 'COMPLIANT'
         return self.build_evaluation(
             item['resourceId'],
             compliance_type,
@@ -124,14 +135,17 @@ class ELBListenerSecurityProtocolDiscouraged(ELBListenerSecurity):
 
     def __init__(self, app):
         self.title = 'ELB Listener Security'
-        self.description = (
-            ''
-        )
+        self.description = 'A load balancer uses a cipher or protocol that is not recommended.'
         self.why_is_it_important = (
-            ''
+            'Vulnerabilities can be found in ciphers and protocols, '
+            'and so they may become deprecated in favour of more secure ones. '
+            'It\'s important to make sure that a listener does not use outdated ciphers or protocols, '
+            'as otherwise they will be insecure.'
         )
         self.how_do_i_fix_it = (
-            ''
+            'Change the security policy on the listener to one that is recommended. '
+            'Further instructions and information can be found here: '
+            'https://docs.aws.amazon.com/elasticloadbalancing/latest/application/create-https-listener.html'
         )
         super(ELBListenerSecurityProtocolDiscouraged, self).__init__(app)
 
@@ -140,9 +154,13 @@ class ELBListenerSecurityProtocolDiscouraged(ELBListenerSecurity):
         TODO
         """
         compliance_type = 'NON_COMPLIANT'
-        self.annotation = (
-            ''
-        )
+        if item["metadata"][4] == 'A listener uses a deprecated cipher or protocol':
+            self.annotation = (
+                f'"{item["metadata"][1]}" load balancer in region "{item["metadata"][0]}" with resource ID: '
+                f'"{item["resourceId"]}" uses a cipher or protocol that is not recommended.'
+            )
+        else:
+            compliance_type = 'COMPLIANT'
         return self.build_evaluation(
             item['resourceId'],
             compliance_type,
@@ -160,14 +178,17 @@ class ELBListenerSecurityInsecureProtocol(ELBListenerSecurity):
 
     def __init__(self, app):
         self.title = 'ELB Listener Security'
-        self.description = (
-            ''
-        )
+        self.description = 'A load balancer uses a cipher or protocol that is not recommended.'
         self.why_is_it_important = (
-            ''
+            'Vulnerabilities can be found in ciphers and protocols, '
+            'and so they may become deprecated in favour of more secure ones. '
+            'It\'s important to make sure that a listener does not use outdated ciphers or protocols, '
+            'as otherwise they will be insecure.'
         )
         self.how_do_i_fix_it = (
-            ''
+            'Change the security policy on the listener to a more recent one. '
+            'Further instructions and information can be found here: '
+            'https://docs.aws.amazon.com/elasticloadbalancing/latest/application/create-https-listener.html'
         )
         super(ELBListenerSecurityInsecureProtocol, self).__init__(app)
 
@@ -176,9 +197,13 @@ class ELBListenerSecurityInsecureProtocol(ELBListenerSecurity):
         TODO
         """
         compliance_type = 'NON_COMPLIANT'
-        self.annotation = (
-            ''
-        )
+        if item["metadata"][3] == 'Red':
+            self.annotation = (
+                f'"{item["metadata"][1]}" load balancer in region "{item["metadata"][0]}" with resource ID: '
+                f'"{item["resourceId"]}" uses an insecure cipher or protocol.'
+            )
+        else:
+            compliance_type = 'COMPLIANT'
         return self.build_evaluation(
             item['resourceId'],
             compliance_type,
