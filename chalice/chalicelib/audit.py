66e39849bcff4e09b1531f522434ec88f57a415a
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2018-12-17 11:24:13 +0000

    Update criteria processed before save

--- chalice/chalicelib/audit.py
+++ chalice/chalicelib/audit.py
@@ -195,9 +195,9 @@ def account_evaluate_criteria(event):
                 audit_criterion.failed = summary['non_compliant']['display_stat']
                 audit_criterion.ignored = summary['not_applicable']['display_stat']
                 audit_criterion.regions = summary['regions']['count']
-                audit_criterion.save()
                 # Only update the processed stat if the assume was successful
                 audit.criteria_processed += 1
+                audit_criterion.save()
 
             audit.date_updated = datetime.now()
             message_data = audit_criterion.serialize()
