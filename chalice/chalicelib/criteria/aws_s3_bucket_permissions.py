2d07c0bfc96c5de3dd73078d3a249aaf6a0ebee6
Argyris Galamatis <argyris.galamatis@digital.cabinet-office.gov.uk>
2018-12-20 09:29:22 +0000

    CT-383/386 s3 bucket permissions super init implemented, unit tests 240 fails and errors

--- chalice/chalicelib/criteria/aws_s3_bucket_permissions.py
+++ chalice/chalicelib/criteria/aws_s3_bucket_permissions.py
@@ -5,6 +5,7 @@ Checks on the S3 bucket permissions that can potentially compromise files.
 """
 
 from chalicelib.criteria.criteria_default import CriteriaDefault
+from chalicelib.aws.gds_support_client import GdsSupportClient
 
 
 class S3BucketPermissions(CriteriaDefault):
@@ -14,6 +15,16 @@ class S3BucketPermissions(CriteriaDefault):
     active = False
 
     def __init__(self, app):
+        # attributes to overwrite in subclasses
+        self.status_string = ''
+        self.status_interval = ''
+        # attributes common in both subclasses
+        self.resource_type = 'AWS::s3::bucket_permissions'
+        self.ClientClass = GdsSupportClient
+        self.check_id = 'Pfx0RwqBli'
+        self.language = 'en'
+        self.region = 'us-east-1'
+        self.annotation = ''
         super(S3BucketPermissions, self).__init__(app)
 
     def get_data(self, session, **kwargs):
