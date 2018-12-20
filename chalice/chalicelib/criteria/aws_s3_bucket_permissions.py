d9049ac05de23ed1e744c07dfc48469ecdfe3b39
Argyris Galamatis <argyris.galamatis@digital.cabinet-office.gov.uk>
2018-12-20 10:01:39 +0000

    CT-383/386 s3 bucket permissions subclasses init methods implemented, unit tests downn to 201 fails and errors

--- chalice/chalicelib/criteria/aws_s3_bucket_permissions.py
+++ chalice/chalicelib/criteria/aws_s3_bucket_permissions.py
@@ -51,6 +51,25 @@ class S3BucketReadAll(S3BucketPermissions):
     active = True
 
     def __init__(self, app):
+        self.title = (
+            'S3 Bucket Permissions: The bucket ACL allows List access '
+            'for "Everyone" or "Any Authenticated AWS User"'
+        )
+        self.description = (
+            'There are S3 buckets with open access permissions '
+            'or allow access to any authenticated AWS user in your account.'
+        )
+        self.why_is_it_important = (
+            'If a bucket has world upload/delete permissions, this allows anyone to create, '
+            'modify and delete files in the S3 bucket; this can clearly cause issues. '
+            'However, even “List” permissions being open to the world can cause problems '
+            '- malicious individuals can rack up costs on a bucket by repeatedly listing documents on a bucket. '
+            'Therefore it’s vital to secure all S3 buckets by making sure that they are closed to everyone outside of GDS.'
+        )
+        self.how_do_i_fix_it = (
+            'Review the permissions on the listed buckets, and change them to make sure '
+            'that they are no longer open.'
+        )
         super(S3BucketReadAll, self).__init__(app)
 
     def evaluate(self, event, item, whitelist=[]):
@@ -64,6 +83,24 @@ class S3BucketOpenAccess(S3BucketPermissions):
     active = True
 
     def __init__(self, app):
+        self.title = (
+            'S3 Bucket Permissions:  A bucket policy allows any kind of open access.'
+        )
+        self.description = (
+            'There are S3 buckets with open access permissions or '
+            'allow access to any authenticated AWS user in your account.'
+        )
+        self.why_is_it_important = (
+            'If a bucket has world upload/delete permissions, this allows anyone to create, '
+            'modify and delete files in the S3 bucket; this can clearly cause issues. '
+            'However, even “List” permissions being open to the world can cause problems '
+            '- malicious individuals can rack up costs on a bucket by repeatedly listing documents on a bucket. '
+            'Therefore it’s vital to secure all S3 buckets by making sure '
+            'that they are closed to everyone outside of GDS.'
+        )
+        self.how_do_i_fix_it = (
+            'Review the permissions on the listed buckets, and change them to make sure that they are no longer open.'
+        )
         super(S3BucketOpenAccess, self).__init__(app)
 
     def evaluate(self, event, item, whitelist=[]):
@@ -77,6 +114,26 @@ class S3BucketWriteAll(S3BucketPermissions):
     active = True
 
     def __init__(self, app):
+        self.title = (
+            'S3 Bucket Permissions:  The bucket ACL allows Upload/Delete access '
+            'for "Everyone" or "Any Authenticated AWS User".'
+        )
+        self.description = (
+            'There are S3 buckets with open access permissions or allow access '
+            'to any authenticated AWS user in your account.'
+        )
+        self.why_is_it_important = (
+            'If a bucket has world upload/delete permissions, this allows anyone to create, '
+            'modify and delete files in the S3 bucket; this can clearly cause issues. '
+            'However, even “List” permissions being open to the world can cause problems '
+            '- malicious individuals can rack up costs on a bucket by repeatedly listing documents on a bucket. '
+            'Therefore it’s vital to secure all S3 buckets by making sure '
+            'that they are closed to everyone outside of GDS.'
+        )
+        self.how_do_i_fix_it = (
+            'Review the permissions on the listed buckets, and change them to make sure that they are no longer open.'
+        )
+        
         super(S3BucketWriteAll, self).__init__(app)
 
     def evaluate(self, event, item, whitelist=[]):
