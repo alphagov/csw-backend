e601c1a4d51d0835498f6097b257ddd813740df3
Argyris Galamatis <argyris.galamatis@digital.cabinet-office.gov.uk>
2019-01-24 13:13:03 +0000

    refined tests, more data to test and implementation

--- chalice/chalicelib/criteria/aws_ec2_egress_restriction.py
+++ chalice/chalicelib/criteria/aws_ec2_egress_restriction.py
@@ -4,17 +4,60 @@ from chalicelib.aws.gds_ec2_security_group_client import GdsEc2SecurityGroupClie
 from chalicelib.criteria.criteria_default import CriteriaDefault
 
 
-class EgressRestrition(CriteriaDefault):
+class UnrestrictedEgressSecurityGroups(CriteriaDefault):
     """
     """
     active = True
 
-    ClientClass = GdsEc2SecurityGroupClient
+    def __init__(self, app):
+        self.ClientClass = GdsEc2SecurityGroupClient
+        self.resource_type = 'AWS::EC2::SecurityGroup'
+        self.annotation = ''
+        self.is_regional = True
+        self.title = 'Unrestricted Egress Security Groups'
+        self.description = (
+            'Checks security groups for rules that allow unrestricted access to a resource.'
+        )
+        self.why_is_it_important = (
+            'Unrestricted access increases opportunities for malicious activity '
+            '(hacking, denial-of-service attacks, loss of data).'
+        )
+        self.how_do_i_fix_it = (
+            'Restrict access to only those IP addresses that require it. <br />'
+            'To restrict access to a specific IP address, set the suffix to /32 ''(for example, 192.0.2.10/32). <br />'
+            'Be sure to delete overly permissive rules after creating rules that are more restrictive <br />. '
+            'Additional Resources:<br />.'
+            '<a href="https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-network-security.html">'
+            'Amazon EC2 Security Groups</a><br />.'
+            '<a href="https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing">'
+            'Classless Inter-Domain Routing</a> (Wikipedia)<br />.'
+        )
+        super(UnrestrictedEgressSecurityGroups, self).__init__(app)
 
     def get_data(self, session, **kwargs):
         return self.client.describe_security_groups(session, **kwargs)
 
+    def translate(self, data):
+        print(data, type(data))
+        return {
+            "resource_id": data['GroupId'],
+            "resource_name": data['GroupName'],
+        }
+
     def evaluate(self, event, item, whitelist=[]):
         """
         """
-        raise
+        compliance_type = 'COMPLIANT'
+        for egress_permission in item['IpPermissionsEgress']:
+            if egress_permission['IpProtocol'] == '-1':
+                for ip_range in egress_permission['IpRanges']:
+                    if ip_range['CidrIp'] == '0.0.0.0/0':
+                        compliance_type = 'NON_COMPLIANT'
+                        self.annotation = 'This security group has unrestricted outbound traffic.'
+        return self.build_evaluation(
+            item['GroupId'],
+            compliance_type,
+            event,
+            self.resource_type,
+            self.annotation
+        )
--- chalice/tests/chalicelib/criteria/test_aws_ec2_egress_restriction.py
+++ chalice/tests/chalicelib/criteria/test_aws_ec2_egress_restriction.py
@@ -1,11 +1,11 @@
-from chalicelib.criteria.aws_ec2_egress_restriction import EgressRestrition
+from chalicelib.criteria.aws_ec2_egress_restriction import UnrestrictedEgressSecurityGroups
 from tests.chalicelib.criteria.test_criteria_default import (
     CriteriaSubclassTestCaseMixin, TestCaseWithAttrAssert
 )
 from tests.chalicelib.criteria.test_data import EGRESS_RESTRICTION
 
 
-class TestEgressRestrition(CriteriaSubclassTestCaseMixin, TestCaseWithAttrAssert):
+class TestUnrestrictedEgressSecurityGroups(CriteriaSubclassTestCaseMixin, TestCaseWithAttrAssert):
     """
     Mixin class for the two key test case classes below
     """
@@ -14,14 +14,14 @@ class TestEgressRestrition(CriteriaSubclassTestCaseMixin, TestCaseWithAttrAssert
     def setUpClass(cls):
         """
         """
-        super(TestEgressRestrition, cls).setUpClass()
+        super(TestUnrestrictedEgressSecurityGroups, cls).setUpClass()
         cls.test_data = EGRESS_RESTRICTION
 
     def setUp(self):
         """
         """
-        super(TestEgressRestrition, self).setUpClass()
-        self.subclass = EgressRestrition(self.app)
+        super(TestUnrestrictedEgressSecurityGroups, self).setUpClass()
+        self.subclass = UnrestrictedEgressSecurityGroups(self.app)
 
     def test_init_client(self):
         """
@@ -34,14 +34,31 @@ class TestEgressRestrition(CriteriaSubclassTestCaseMixin, TestCaseWithAttrAssert
         """
         # overwrite the client.describe_trusted_advisor_check_result(...) to return a static response
         self.subclass.client.describe_security_groups = \
-            lambda session: EGRESS_RESTRICTION['pass']
+            lambda session: EGRESS_RESTRICTION['fail']['SecurityGroups']
         # output value
         item = self.subclass.get_data(None)
         # must return a dictionary with the three necessary keys
         msg = "the method must return a list of dictionaries"
-        print(item)
         with self.subTest():
-            self.assertIsInstance(item, dict, msg=msg)
+            self.assertIsInstance(item, list, msg=msg)
+
+    def test_translate(self):
+        """
+        """
+        # output value
+        for case in EGRESS_RESTRICTION:
+            for group in EGRESS_RESTRICTION[case]['SecurityGroups']:
+                translation = self.subclass.translate(group)
+                self.assertEqual(
+                    translation['resource_id'],
+                    group['GroupId'],
+                    msg='The resource ID does not match the Security Group ID.'
+                )
+                self.assertEqual(
+                    translation['resource_name'],
+                    group['GroupName'],
+                    msg='The resource name does not match the Security Group name.'
+                )
 
     def test_evaluate_pass(self):
         """
@@ -49,7 +66,7 @@ class TestEgressRestrition(CriteriaSubclassTestCaseMixin, TestCaseWithAttrAssert
         # input params
         event = {}
         whitelist = []
-        for item in EGRESS_RESTRICTION['pass']:
+        for item in EGRESS_RESTRICTION['pass']['SecurityGroups']:
             # tests
             output = self._evaluate_invariant_assertions(event, item, whitelist)
             self._evaluate_passed_status_assertions(item, output)
@@ -60,8 +77,7 @@ class TestEgressRestrition(CriteriaSubclassTestCaseMixin, TestCaseWithAttrAssert
         # input params
         event = {}
         whitelist = []
-        for item in EGRESS_RESTRICTION['fail']:
+        for item in EGRESS_RESTRICTION['fail']['SecurityGroups']:
             # tests
             output = self._evaluate_invariant_assertions(event, item, whitelist)
             self._evaluate_failed_status_assertions(item, output)
-
