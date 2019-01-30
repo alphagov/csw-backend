152e4aa65cc029e81d5b47ffaa8ac1bedbababc1
Argyris Galamatis <argyris.galamatis@digital.cabinet-office.gov.uk>
2019-01-30 11:21:40 +0000

    CT-484 refined unit tests

--- chalice/tests/chalicelib/criteria/test_aws_iam_access_key_rotation.py
+++ chalice/tests/chalicelib/criteria/test_aws_iam_access_key_rotation.py
@@ -9,9 +9,11 @@ from tests.chalicelib.criteria.test_criteria_default import (
 from tests.chalicelib.criteria.test_data import IAM_KEY_ROTATION_ITEMS
 
 
-class TestAwsIamAccessKeyRotationMixin(CriteriaSubclassTestCaseMixin):
+class TestAwsIamAccessKeyRotationYellow(
+    CriteriaSubclassTestCaseMixin, TestCaseWithAttrAssert
+):
     """
-    Unit tests for the CriteriaDefault class
+    Unit tests for the Yellow subclass
     """
 
     @classmethod
@@ -19,8 +21,27 @@ class TestAwsIamAccessKeyRotationMixin(CriteriaSubclassTestCaseMixin):
         """
         initialise the the Chalice app objects once to reuse it in every test.
         """
-        super(TestAwsIamAccessKeyRotationMixin, cls).setUpClass()
+        super(TestAwsIamAccessKeyRotationYellow, cls).setUpClass()
         cls.test_data = IAM_KEY_ROTATION_ITEMS
+
+    def setUp(self):
+        """
+        """
+        self.subclass = AwsIamAccessKeyRotationYellow(self.app)
+
+    def test_init_success(self):
+        """
+        test that initialization works
+        """
+        self.assertIsInstance(
+            self.subclass, AwsIamAccessKeyRotationYellow
+        )
+
+    def test_init_failure(self):
+        """
+        test that not passing Chalice app object raises a type error
+        """
+        self.assertRaises(TypeError, AwsIamAccessKeyRotationYellow)
 
     def test_init_client(self):
         """
@@ -41,93 +62,26 @@ class TestAwsIamAccessKeyRotationMixin(CriteriaSubclassTestCaseMixin):
         msg = "the method must return a list of dictionaries"
         with self.subTest():
             self.assertIsInstance(item, list, msg=msg)
-        # with self.subTest():
-        #     self.assertIsInstance(
-        #         item['result']['status'],
-        #         dict,
-        #         msg=msg
-        #     )
 
-
-class TestAwsIamAccessKeyRotationYellow(
-    TestAwsIamAccessKeyRotationMixin, TestCaseWithAttrAssert
-):
-    """
-    Unit tests for the Yellow subclass
-    """
-
-    @classmethod
-    def setUpClass(cls):
-        """
-        initialise the the Chalice app objects once to reuse it in every test.
-        """
-        super(TestAwsIamAccessKeyRotationYellow, cls).setUpClass()
-
-    def setUp(self):
-        """
-        """
-        self.subclass = AwsIamAccessKeyRotationYellow(self.app)
-
-    def test_init_success(self):
-        """
-        test that initialization works
-        """
-        self.assertIsInstance(
-            self.subclass, AwsIamAccessKeyRotationYellow
-        )
-
-    def test_init_failure(self):
-        """
-        test that not passing Chalice app object raises a type error
-        """
-        self.assertRaises(TypeError, AwsIamAccessKeyRotationYellow)
-
-    def test_evaluate_green(self):
-        """
-        green (status: ok) test
-        """
-        # input params
-        event = {}
-        whitelist = []
-        for item in self.test_data['green']['result']['flaggedResources']:
-            # tests
-            output = self._evaluate_invariant_assertions(event, item, whitelist)
-            self._evaluate_passed_status_assertions(item, output)
-
-    def test_evaluate_yellow(self):
+    def test_evaluate(self):
         """
         yellow (status: warning) test
         """
         # input params
         event = {}
         whitelist = []
-        for item in self.test_data['yellow']['result']['flaggedResources']:
-            # tests
-            output = self._evaluate_invariant_assertions(event, item, whitelist)
-            if item['status'] == 'warning':
-                self._evaluate_failed_status_assertions(item, output)
-            else:
-                self._evaluate_passed_status_assertions(item, output)
-
-    def test_evaluate_red(self):
-        """
-        red (status: error) test
-        """
-        # input params
-        event = {}
-        whitelist = []
-        for item in self.test_data['red']['result']['flaggedResources']:
-            # tests
-            output = self._evaluate_invariant_assertions(event, item, whitelist)
-            if item['status'] == 'error':
-                self._evaluate_failed_status_assertions(item, output)
-            else:
-                self._evaluate_passed_status_assertions(item, output)
+        for suite in self.test_data:
+            for item in self.test_data[suite]['result']['flaggedResources']:
+                with self.subTest():
+                    # tests
+                    output = self._evaluate_invariant_assertions(event, item, whitelist)
+                    if item['status'] in ['warning', 'error']:
+                        self._evaluate_failed_status_assertions(item, output)
+                    else:
+                        self._evaluate_passed_status_assertions(item, output)
 
 
-class TestAwsIamAccessKeyRotationRed(
-    TestAwsIamAccessKeyRotationMixin, TestCaseWithAttrAssert
-):
+class TestAwsIamAccessKeyRotationRed(TestAwsIamAccessKeyRotationYellow):
     """
     Unit tests for the Red subclass
     """
@@ -158,116 +112,21 @@ class TestAwsIamAccessKeyRotationRed(
         """
         self.assertRaises(TypeError, AwsIamAccessKeyRotationRed)
 
-
-class TestAwsIamAccessKeyRotationYellow(
-    TestAwsIamAccessKeyRotationMixin, TestCaseWithAttrAssert
-):
-    """
-    Unit tests for the Yellow subclass
-    """
-
-    @classmethod
-    def setUpClass(cls):
-        """
-        initialise the the Chalice app objects once to reuse it in every test.
-        """
-        super(TestAwsIamAccessKeyRotationYellow, cls).setUpClass()
-
-    def setUp(self):
-        """
-        """
-        self.subclass = AwsIamAccessKeyRotationYellow(self.app)
-
-    def test_init_success(self):
-        """
-        test that initialization works
-        """
-        self.assertIsInstance(
-            self.subclass, AwsIamAccessKeyRotationYellow
-        )
-
-    def test_init_failure(self):
-        """
-        test that not passing Chalice app object raises a type error
-        """
-        self.assertRaises(TypeError, AwsIamAccessKeyRotationYellow)
-
-    def test_evaluate_green(self):
-        """
-        green (status: ok) test
-        """
-        # input params
-        event = {}
-        whitelist = []
-        for item in self.test_data['green']['result']['flaggedResources']:
-            # tests
-            output = self._evaluate_invariant_assertions(event, item, whitelist)
-            self._evaluate_passed_status_assertions(item, output)
-
-    def test_evaluate_yellow(self):
+    def test_evaluate(self):
         """
         yellow (status: warning) test
         """
         # input params
         event = {}
         whitelist = []
-        for item in self.test_data['yellow']['result']['flaggedResources']:
-            # tests
-            output = self._evaluate_invariant_assertions(event, item, whitelist)
-            if item['status'] == 'warning':
-                self._evaluate_failed_status_assertions(item, output)
-            else:
-                self._evaluate_passed_status_assertions(item, output)
-
-    def test_evaluate_red(self):
-        """
-        red (status: error) test
-        """
-        # input params
-        event = {}
-        whitelist = []
-        for item in self.test_data['red']['result']['flaggedResources']:
-            # tests
-            output = self._evaluate_invariant_assertions(event, item, whitelist)
-            if item['status'] == 'error':
-                self._evaluate_failed_status_assertions(item, output)
-            else:
-                self._evaluate_passed_status_assertions(item, output)
+        for suite in self.test_data:
+            for item in self.test_data[suite]['result']['flaggedResources']:
+                with self.subTest():
+                    # tests
+                    output = self._evaluate_invariant_assertions(event, item, whitelist)
+                    if item['status'] == 'error':
+                        self._evaluate_failed_status_assertions(item, output)
+                    else:
+                        self._evaluate_passed_status_assertions(item, output)
 
 
-class TestAwsIamAccessKeyRotationRed(
-    TestAwsIamAccessKeyRotationMixin, TestCaseWithAttrAssert
-):
-    """
-    Unit tests for the Red subclass
-    """
-
-    @classmethod
-    def setUpClass(cls):
-        """
-        initialise the the Chalice app objects once to reuse it in every test.
-        """
-        super(TestAwsIamAccessKeyRotationRed, cls).setUpClass()
-
-    def setUp(self):
-        """
-        """
-        self.subclass = AwsIamAccessKeyRotationRed(self.app)
-
-    def test_init_success(self):
-        """
-        test that initialization works
-        """
-        self.assertIsInstance(
-            self.subclass, AwsIamAccessKeyRotationRed
-        )
-
-    def test_init_failure(self):
-        """
-        test that not passing Chalice app object raises a type error
-        """
-        self.assertRaises(TypeError, AwsIamAccessKeyRotationRed)
-
-
-if __name__ == '__main__':
-    unittest.main()
