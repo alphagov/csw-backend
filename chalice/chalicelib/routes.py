70365b9707ec8ce53ed2007e03395c9834151100
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-02-04 07:45:41 +0000

    WIP: Add authentication test on routes

--- chalice/chalicelib/routes.py
+++ chalice/chalicelib/routes.py
@@ -131,11 +131,13 @@ def team_dashboard(id):
     # TODO - add check user has access to team
     load_route_services()
     try:
+        authed = app.auth.try_login(app.current_request)
+        team = models.ProductTeam.get_by_id(team_id)
+
         if authed:
             user_data = app.auth.get_login_data()
             user = models.User.find_active_by_email(user_data['email'])
 
-            team = models.ProductTeam.get_by_id(team_id)
             if (team.user_has_access(user)):
                 app.log.debug("Team: " + app.utilities.to_json(team))
                 criteria_stats = models.ProductTeam.get_criteria_stats([team])
@@ -151,16 +153,10 @@ def team_dashboard(id):
                     }
                 )
             else:
-                response = app.templates.render_authorized_template(
-                    'denied.html',
-                    app.current_request,
-                    {
-                        "refused": {
-                            "type": "team",
-                            "requested": team.serialize()
-                        }
-                    }
-                )
+                response = app.templates.refuse(app.current_request, "team", team.get_item())
+        else:
+            response = app.templates.refuse(app.current_request, "team", team.get_item())
+
     except Exception as err:
         app.log.error("Route: team dashboard error: " + str(err))
         response = app.templates.default_server_error()
