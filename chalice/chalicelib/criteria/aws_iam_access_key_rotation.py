fe0b752f4b4844a8ee7b3603769dc36740b97727
Argyris Galamatis <argyris.galamatis@digital.cabinet-office.gov.uk>
2019-02-05 15:57:52 +0000

    CT-483 replaced uninitialised variables in formated strings with constants in both final classes

--- chalice/chalicelib/criteria/aws_iam_access_key_rotation.py
+++ chalice/chalicelib/criteria/aws_iam_access_key_rotation.py
@@ -15,27 +15,12 @@ class AwsIamAccessKeyRotationBase(TrustedAdvisorCriterion):
     def __init__(self, app):
         self.resource_type = 'AWS::iam::access_key_rotation'
         self.check_id = 'DqdJqYeRm5'
-        super(AwsIamAccessKeyRotationBase, self).__init__(app)  # moved to define status_interval before using it below
         self.title = " Outdated IAM access keys"
-        self.description = (
-            'At least one active Identity and Access Management '
-            f'(IAM) access key has not been rotated in the last {self.status_interval}.'
-        )
         self.why_is_it_important = (
             'Rotating IAM credentials periodically will significantly reduce the chances that a compromised set of '
             'access keys can be used without your knowledge to access certain components within your AWS account.'
         )
-        self.how_do_i_fix_it = (
-            'Ensure that all your IAM user access keys are rotated at least every {self.status_interval} in order to  '
-            'decrease the likelihood of accidental exposures and protect your AWS resources '
-            'against unauthorized access. <br />To rotate access keys, it is recommended to follow these steps: <ol>'
-            '<li>Create a second access key in addition to the one in use.</li>'
-            '<li>Update all your applications to use the new access key and validate that the applications are working.</li>'
-            '<li>Change the state of the previous access key to inactive.</li>'
-            '<li>Validate that your applications are still working as expected.</li>'
-            '<li>Delete the inactive access key.</li>'
-            '</ol>'
-        )
+        super(AwsIamAccessKeyRotationBase, self).__init__(app)
 
     def translate(self, data={}):
         """
@@ -55,8 +40,21 @@ class AwsIamAccessKeyRotationYellow(AwsIamAccessKeyRotationBase):
 
     def __init__(self, app):
         super(AwsIamAccessKeyRotationYellow, self).__init__(app)
-        self.status_string = 'Yellow'
-        self.status_interval = '90 days'
+        self.description = (
+            'At least one active Identity and Access Management '
+            f'(IAM) access key has not been rotated in the last 90 days.'
+        )
+        self.how_do_i_fix_it = (
+            'Ensure that all your IAM user access keys are rotated at least every 90 days in order to  '
+            'decrease the likelihood of accidental exposures and protect your AWS resources '
+            'against unauthorized access. <br />To rotate access keys, it is recommended to follow these steps: <ol>'
+            '<li>Create a second access key in addition to the one in use.</li>'
+            '<li>Update all your applications to use the new access key and validate that the applications are working.</li>'
+            '<li>Change the state of the previous access key to inactive.</li>'
+            '<li>Validate that your applications are still working as expected.</li>'
+            '<li>Delete the inactive access key.</li>'
+            '</ol>'
+        )
 
     def evaluate(self, event, item, whitelist=[]):
         """
@@ -69,7 +67,7 @@ class AwsIamAccessKeyRotationYellow(AwsIamAccessKeyRotationBase):
             compliance_type = 'NON_COMPLIANT'
             self.annotation = (
                 f'User "{item["metadata"][1]}" has not rotated access key '
-                f'"{item["metadata"][2]}" for more than {self.status_interval}'
+                f'"{item["metadata"][2]}" for more than 90 days'
             )
         else:
             compliance_type = 'COMPLIANT'
@@ -91,8 +89,21 @@ class AwsIamAccessKeyRotationRed(AwsIamAccessKeyRotationBase):
 
     def __init__(self, app):
         super(AwsIamAccessKeyRotationRed, self).__init__(app)
-        self.status_string = 'Red'
-        self.status_interval = '2 years'
+        self.description = (
+            'At least one active Identity and Access Management '
+            f'(IAM) access key has not been rotated in the last 2 years.'
+        )
+        self.how_do_i_fix_it = (
+            'Ensure that all your IAM user access keys are rotated at least every 2 years in order to  '
+            'decrease the likelihood of accidental exposures and protect your AWS resources '
+            'against unauthorized access. <br />To rotate access keys, it is recommended to follow these steps: <ol>'
+            '<li>Create a second access key in addition to the one in use.</li>'
+            '<li>Update all your applications to use the new access key and validate that the applications are working.</li>'
+            '<li>Change the state of the previous access key to inactive.</li>'
+            '<li>Validate that your applications are still working as expected.</li>'
+            '<li>Delete the inactive access key.</li>'
+            '</ol>'
+        )
 
     def evaluate(self, event, item, whitelist=[]):
         """
@@ -106,7 +117,7 @@ class AwsIamAccessKeyRotationRed(AwsIamAccessKeyRotationBase):
             compliance_type = 'NON_COMPLIANT'
             self.annotation = (
                 f'User "{item["metadata"][1]}" has not rotated access key '
-                f'"{item["metadata"][2]}" for more than {self.status_interval}'
+                f'"{item["metadata"][2]}" for more than 2 years'
             )
         return self.build_evaluation(
             item['resourceId'],
