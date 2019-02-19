4206352c66800af7289bcd477dcfffa920033146
Argyris Galamatis <argyris.galamatis@digital.cabinet-office.gov.uk>
2019-02-19 11:43:45 +0000

    CT-499 KMS client class

---
+++ chalice/chalicelib/aws/gds_kms_client.py
@@ -0,0 +1,70 @@
+"""
+GdsKmsClient
+extends GdsAwsClient
+implements aws Key Management Service endpoint queries
+"""
+from chalicelib.aws.gds_aws_client import GdsAwsClient
+
+
+class GdsKmsClient(GdsAwsClient):
+    """
+    """
+
+    def get_key_list_with_details(self, session):
+        keys_list = self.get_key_list(session)
+        for key in keys_list:
+            key.update(self.get_key_rotation_status(session, key['KeyArn']))
+            key.update({'KeyRotationEnabled': self.get_key_details(session, key['KeyArn']['KeyRotationEnabled'])})
+        return keys_list
+
+    def get_key_list(self, session):
+        """
+        """
+        try:
+            kms_client = self.get_boto3_session_client('kms', session)
+            return kms_client.list_keys().get('Keys', [])
+        except Exception as exception:
+            return str(exception)
+
+    def get_key_rotation_status(self, session, key_id_or_arn):
+        """
+        """
+        try:
+            kms_client = self.get_boto3_session_client('kms', session)
+            return kms_client.get_key_rotation_status(KeyId=key_id_or_arn)['KeyRotationEnabled']
+        except Exception as exception:
+            return str(exception)
+
+    def get_key_details(self, session, key_id_or_arn):
+        """
+        """
+        try:
+            kms_client = self.get_boto3_session_client('kms', session)
+            return kms_client.describe_key(KeyId=key_id_or_arn).get('KeyMetadata', {})
+        except Exception as exception:
+            return str(exception)
+
+    ###
+    # methods for testing, not allowed by users' policies
+    ###
+
+    def create_key(self, session):
+        """
+        """
+        try:
+            kms_client = self.get_boto3_session_client('kms', session)
+            return kms_client.create_key().get('KeyMetadata', {})
+        except Exception as exception:
+            return str(exception)
+
+    def schedule_key_deletion(self, session, key_id_or_arn, days=7):
+        """
+        The number of days that the key can be deleted can be from 7 to 30, inlcusive.
+        """
+        try:
+            kms_client = self.get_boto3_session_client('kms', session)
+            return kms_client.schedule_key_deletion(
+                KeyId=key_id_or_arn, PendingWindowInDays=days
+            ).get('KeyMetadata', {})
+        except Exception as exception:
+            return str(exception)
