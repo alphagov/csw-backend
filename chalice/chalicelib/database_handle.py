9d30555c9173615a3bbbe668c5f1af65a486741d
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-02-27 13:35:44 +0000

    issubclass is compares sub-class to class not instance to class.

--- chalice/chalicelib/database_handle.py
+++ chalice/chalicelib/database_handle.py
@@ -293,7 +293,7 @@ class BaseModel(peewee.Model):
 
         for field in fields:
             value = data.get(field, None)
-            if issubclass(value, BaseModel):
+            if issubclass(type(value), BaseModel):
                 raw_data[field] = value.id
             else:
                 raw_data[field] = value
