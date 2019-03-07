154c82ca8086b2d5aa85f1840bb918af2b2707c9
Argyris Galamatis <argyris.galamatis@digital.cabinet-office.gov.uk>
2019-03-07 13:20:02 +0000

    CT-555 client class

---
+++ chalice/chalicelib/aws/gds_rds_client.py
@@ -0,0 +1,21 @@
+"""
+GdsRdsClient
+extends GdsAwsClient
+implements aws Key Management Service endpoint queries
+"""
+from chalicelib.aws.gds_aws_client import GdsAwsClient
+
+
+class GdsRdsClient(GdsAwsClient):
+    """
+    """
+
+    def describe_db_instances(self, session):
+        """
+        """
+        kms_client = self.get_boto3_session_client('rds', session)
+        data = kms_client.describe_db_instances().get('DBInstances', [])
+        self.app.log.debug('RDS::get_boto3_session_client')
+        self.app.log.debug(type(data))
+        self.app.log.debug(data)
+        return data
