dd13a8b8bf4cdc3c2dbfa45c1e9506df0b9b5fac
Argyris Galamatis <argyris.galamatis@digital.cabinet-office.gov.uk>
2019-03-07 14:08:36 +0000

    CT-555 criterion class attributes

--- chalice/chalicelib/criteria/aws_rds_encryption.py
+++ chalice/chalicelib/criteria/aws_rds_encryption.py
@@ -7,6 +7,35 @@ from chalicelib.criteria.criteria_default import CriteriaDefault
 class RdsEncryption(CriteriaDefault):
     """
     """
+    active = True
+    ClientClass = GdsRdsClient
+    is_regional = False
+    resource_type = 'AWS::RDS::ENCRYPTION'
+    title = 'Relational Database Service: Encryption Enabled'
+    description = (
+        'The data stored on the following RDS resources is not encrypted.'
+    )
+    why_is_it_important = (
+        'Enabling encryption in RDS resources protects sensitive data stored in your database '
+        'from unauthorized access or theft.'
+    )
+    how_do_i_fix_it = (
+        'To enable encryption to new RDS database instance, follow these '
+        '<a href="https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Overview.Encryption.html#Overview.Encryption.Enabling">instructions</a>.<br />'
+        'To enable RDS encryption to an existing unencrypted database, '
+        'create a snapshot of your DB instance, '
+        'and then create an encrypted copy of that snapshot. <br />'
+        'You can then restore a DB instance from the encrypted snapshot, '
+        'and thus you have an encrypted copy of your original DB instance.<br />'
+        '<b>Note:</b> Once you have tested the new encrypted RDS copy, '
+        'consider removing the original unencrypted database '
+        'and all previous snapshots created when encryption was disabled.<br />'
+        'Further information can be found at following AWS links: '
+        '<a href="https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Overview.Encryption.html">Encrypting RDS resources</a>, '
+        '<a href="https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_CopySnapshot.html">Copy a Snapshot</a>'
+    )
+
+    
     def get_data(self, session, **kwargs):
         pass
 
