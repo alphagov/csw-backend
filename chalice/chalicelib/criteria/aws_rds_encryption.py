b5a9aae531c022e95d21fd1cf48c1e9d4133c493
Argyris Galamatis <argyris.galamatis@digital.cabinet-office.gov.uk>
2019-03-07 13:21:59 +0000

    CT-555 criterion declarations

---
+++ chalice/chalicelib/criteria/aws_rds_encryption.py
@@ -0,0 +1,17 @@
+"""
+"""
+from chalicelib.aws.gds_rds_client import GdsRdsClient
+from chalicelib.criteria.criteria_default import CriteriaDefault
+
+
+class RdsEncryption(CriteriaDefault):
+    """
+    """
+    def get_data(self, session, **kwargs):
+        pass
+
+    def translate(self, data={}):
+        pass
+
+    def evaluate(self, event, item, whitelist=[]):
+        pass
