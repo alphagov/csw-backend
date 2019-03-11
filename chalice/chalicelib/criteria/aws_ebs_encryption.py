a1c7a15c6eef39101c07057efc924d0a75e7dd70
Argyris Galamatis <argyris.galamatis@digital.cabinet-office.gov.uk>
2019-03-11 12:18:14 +0000

    CT-586 criterion declarations and unit test file

---
+++ chalice/chalicelib/criteria/aws_ebs_encryption.py
@@ -0,0 +1,43 @@
+"""
+"""
+from chalicelib.aws.gds_ec2_client import GdsEc2Client
+from chalicelib.criteria.criteria_default import CriteriaDefault
+
+
+class RdsEncryption(CriteriaDefault):
+    """
+    """
+    active = True
+    ClientClass = GdsRdsClient
+    is_regional = True
+    resource_type = 'AWS::EBS::ENCRYPTION'
+    title = 'EBS: Encryption Enabled'
+    description = (
+        ''
+    )
+    why_is_it_important = (
+        ''
+    )
+    how_do_i_fix_it = (
+        ''
+    )
+
+    def get_data(self, session, **kwargs):
+        return
+
+    def translate(self, data={}):
+        return {
+            'region': data['TODO::REGION'],
+            'resource_id': data['TODO::ID'],
+            'resource_name': data['TODO::NAME'],
+        }
+
+    def evaluate(self, event, item, whitelist=[]):
+        compliance_type = ''
+        return self.build_evaluation(
+            'TODO::ID',
+            compliance_type,
+            event,
+            self.resource_type,
+            self.annotation
+        )
