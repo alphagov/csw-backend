cc087457410e05d0031b340fe08fb7c379775445
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-03-19 14:20:24 +0000

    Debug log back link address

--- chalice/chalicelib/template_handler.py
+++ chalice/chalicelib/template_handler.py
@@ -329,6 +329,7 @@ class TemplateHandler:
             # Don't populate back link if it links to the current page.
             # For forms which post to themselves
             if "referer" in req.headers and req.headers['referer'] != route:
+                self.app.log.debug("Backlink: " + req.headers['referer'] + " <> " + route)
                 data["back_link"] = req.headers['referer']
 
             response_body = self.render_template(template_file, data)
