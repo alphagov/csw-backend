9b90a97b0c4d87aea74a7f44e70ff30c9f8a913e
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-04-26 16:55:16 +0100

    Add pe-charts, concat js gulp task and load concated js into root template.

--- build/gulp_tasks/7.copy.assets.js
+++ build/gulp_tasks/7.copy.assets.js
@@ -2,7 +2,8 @@
     Copy the relevant parts of the govuk-frontend codebase into the deployed
     chalicelib directory
  */
-var gulp = require('gulp');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
 
 gulp.task('copy.assets_files', function() {
   // place code for your default task here
@@ -14,6 +15,21 @@ gulp.task('copy.assets_scripts', function() {
   return gulp.src(['node_modules/govuk-frontend/*.js'])
   .pipe(gulp.dest('../chalice/chalicelib/assets/govuk-frontend'))
 });
+
+
+gulp.task('concat.js', function() {
+    return gulp.src([
+        './node_modules/d3/dist/d3.min.js',
+        './node_modules/c3/c3.min.js',
+        './node_modules/vue/dist/vue.js',
+        './node_modules/govuk-frontend/all.js',
+        './node_modules/pe-charts/lib/js/app.js',
+        './node_modules/pe-charts/lib/js/table-chart.js'
+    ])
+    .pipe(concat('dist.js'))
+    .pipe(gulp.dest('../chalice/chalicelib/assets'));
+});
+
 
 gulp.task('copy.assets', gulp.series(
     'copy.assets_files',
