8cdd7b8c7d72057100b7ea1999843f9bfa25e8b7
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-04-29 17:03:42 +0100

    Separate head and body js concats and load govuk init from asset rather than inline js.

--- build/gulp_tasks/7.copy.assets.js
+++ build/gulp_tasks/7.copy.assets.js
@@ -17,19 +17,31 @@ gulp.task('copy.assets_scripts', function() {
 });
 
 
-gulp.task('concat.js', function() {
+gulp.task('concat.js.head', function() {
     return gulp.src([
         './node_modules/d3/dist/d3.min.js',
         './node_modules/c3/c3.min.js',
         './node_modules/vue/dist/vue.js',
         './node_modules/govuk-frontend/all.js',
-        './node_modules/pe-charts/lib/js/table-chart.js',
-        './node_modules/pe-charts/lib/js/app.js'
+        './node_modules/pe-charts/lib/js/table-chart.js'
     ])
-    .pipe(concat('dist.js'))
+    .pipe(concat('dist.head.js'))
     .pipe(gulp.dest('../chalice/chalicelib/assets'));
 });
 
+gulp.task('concat.js.body', function() {
+    return gulp.src([
+        './js/govuk-init.js',
+        './node_modules/pe-charts/lib/js/app.js'
+    ])
+    .pipe(concat('dist.body.js'))
+    .pipe(gulp.dest('../chalice/chalicelib/assets'));
+});
+
+gulp.task('concat.js', gulp.series(
+    'concat.js.head',
+    'concat.js.body'
+));
 
 gulp.task('copy.assets', gulp.series(
     'copy.assets_files',
