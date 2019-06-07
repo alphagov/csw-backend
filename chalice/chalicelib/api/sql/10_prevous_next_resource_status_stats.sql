f1f14b2ab2a504055c6c8fa856567fc29a6dc26d
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-06-07 12:38:07 +0100

    Add derived data table to collate previous and next resource statuses

--- chalice/chalicelib/api/sql/10_prevous_next_resource_status_stats.sql
+++ chalice/chalicelib/api/sql/10_prevous_next_resource_status_stats.sql
@@ -2,13 +2,14 @@ DROP TABLE IF EXISTS public._previous_next_resource_status_stats;
 
 CREATE TABLE public._previous_next_resource_status_stats AS
 SELECT
-	seq.prev_id,
 	prev_res.resource_persistent_id,
+	prev_res.criterion_id,
+	seq.prev_id AS prev_audit_id,
 	prev_aud.date_started AS prev_date,
 	prev_aud.finished AS prev_audit_finished,
 	prev_res.id AS prev_audit_resource_id,
 	prev_comp.status_id AS prev_status_id,
-	seq.next_id,
+	seq.next_id AS next_audit_id,
 	next_aud.date_started AS next_date,
 	next_aud.finished AS next_audit_finished,
 	next_res.id AS next_audit_resource_id,
@@ -31,5 +32,8 @@ ON next_res.id = next_comp.audit_resource_id
 WHERE prev_res.resource_persistent_id IS NOT NULL;
 
 CREATE INDEX previous_next_resource_status_stats__resource_persistent_id ON public._previous_next_resource_status_stats (resource_persistent_id);
-CREATE INDEX previous_next_resource_status_stats__prev_id ON public._previous_next_resource_status_stats (prev_id);
-CREATE INDEX previous_next_resource_status_stats__next_id ON public._previous_next_resource_status_stats (next_id);
+CREATE INDEX previous_next_resource_status_stats__criterion_id ON public._previous_next_resource_status_stats (criterion_id);
+CREATE INDEX previous_next_resource_status_stats__prev_audit_id ON public._previous_next_resource_status_stats (prev_audit_id);
+CREATE INDEX previous_next_resource_status_stats__next_audit_id ON public._previous_next_resource_status_stats (next_audit_id);
+CREATE INDEX previous_next_resource_status_stats__prev_audit_resource_id ON public._previous_next_resource_status_stats (prev_audit_resource_id);
+CREATE INDEX previous_next_resource_status_stats__next_audit_resource_id ON public._previous_next_resource_status_stats (next_audit_resource_id);
