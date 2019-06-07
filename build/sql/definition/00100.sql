43067532b1c3b7d2e6dc6cba3a2f4eb07ef92105
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-06-07 14:48:04 +0100

    Added this file in mistaken post rebase merge

--- build/sql/definition/00100.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS _health_metrics(
-    id SERIAL PRIMARY KEY,
-    "name" VARCHAR(200) NOT NULL,
-    "desc" TEXT NOT NULL,
-    "metric_type" VARCHAR(200) NOT NULL,
-    "data" FLOAT NOT NULL,
-    CONSTRAINT health_metrics_name_unique UNIQUE (name)
-);
