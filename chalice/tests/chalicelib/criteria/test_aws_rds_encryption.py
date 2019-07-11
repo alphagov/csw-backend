ecdbbf24a30092d110741fbd0b54643f4b6d14fe
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-07-11 15:38:53 +0100

    Unit test to ensure that stopped and deleting instance statuses pass the check evaluation

--- chalice/tests/chalicelib/criteria/test_aws_rds_encryption.py
+++ chalice/tests/chalicelib/criteria/test_aws_rds_encryption.py
@@ -118,3 +118,19 @@ class TestRdsEncryption(CriteriaSubclassTestCaseMixin, TestCaseWithAttrAssert):
             with self.subTest(key=d):
                 output = self._evaluate_invariant_assertions({}, d, [])
                 self._evaluate_passed_status_assertions(d, output)
+
+    def test_evaluate_stopped(self):
+        for d in self.test_data["fail"]["DBInstances"]:
+            # change item status to check the evaluation changes
+            d["DBInstanceStatus"] = "stopped"
+            with self.subTest(key=d):
+                output = self._evaluate_invariant_assertions({}, d, [])
+                self._evaluate_passed_status_assertions(d, output)
+
+    def test_evaluate_deleting(self):
+        for d in self.test_data["fail"]["DBInstances"]:
+            # change item status to check the evaluation changes
+            d["DBInstanceStatus"] = "deleting"
+            with self.subTest(key=d):
+                output = self._evaluate_invariant_assertions({}, d, [])
+                self._evaluate_passed_status_assertions(d, output)
