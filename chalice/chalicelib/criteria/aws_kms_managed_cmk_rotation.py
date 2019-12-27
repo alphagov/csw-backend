81e876ac901cf48b8697fde4a05f2df28008b213
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2019-12-27 13:58:46 +0000

    Change description of KMS check to be more descriptive

--- chalice/chalicelib/criteria/aws_kms_managed_cmk_rotation.py
+++ chalice/chalicelib/criteria/aws_kms_managed_cmk_rotation.py
@@ -17,16 +17,10 @@ class ManagedCmkRotation(CriteriaDefault):
         "Key Management Service: Customer Master Keys rotated annually by the service"
     )
     description = (
-        "Cryptographic best practices discourage extensive reuse of encryption keys. "
-        "To create new cryptographic material for your Key Management Service (KMS) customer master keys (CMKs), "
-        "you can create new CMKs, and then change your applications "
-        "or aliases to use the new CMKs or you can enable automatic key rotation for an existing CMK. "
-        "Automatic key rotation for a customer managed CMK generates new cryptographic material for it every year. "
-        "KMS also saves the CMK's older cryptographic material to be used to decrypt data that it encrypted.<br />"
-        "Key rotation changes only the CMK's backing key, "
-        "which is the cryptographic material that is used in encryption operations. "
-        "The CMK is the same logical resource, regardless of whether or how many times its backing key changes. "
-        "The properties of the CMK do not change, as shown in the following image."
+        "Checks whether a Customer Managed Key (CMK) has automatic key rotation enabled. If not, "
+        "this can either be because automatic rotation was not turned on, or the CMK is of a "
+        "type that does not support automatic key rotation, such as a CMK with imported key "
+        "material."
     )
     why_is_it_important = (
         "<ul>"
